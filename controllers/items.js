3aa6a239b6ae8ae882d363f1fe25cf1596ab78dd
safakhalaf026 <safakhalaf026@gmail.com>
2025-12-12 22:53:14 +0300

    added GET /:id and GET/:id/edit routes

--- controllers/items.js
+++ controllers/items.js
@@ -4,20 +4,21 @@ const router = express.Router()
 const User = require('../models/user')
 const Item = require('../models/item')
 
-// // Full index (Show More)
-// router.get('/', async(req,res)=>{
-//     try {
-//         // logic here
-//     } catch (error) {
-//         console.error('The following error was encountered:' + error)
-//     }
-// })
+// Full index (Show More)
+router.get('/', async(req,res)=>{
+    try {
+        const allItems = await Item.find().populate('owner')
+        res.render('items/itemIndex.ejs', {allItems})
+    } catch (error) {
+        console.error('The following error was encountered:' + error)
+    }
+})
 
 // User full index (Show All)
 router.get('/mine', async(req,res)=>{
     try {
-        const userItems = await Item.find({}).populate('owner')
-        res.render('items/userIndex.ejs', {userItems})
+        const userItems = await Item.find({owner: req.session.user._id}).populate('owner')
+        res.render('items/userIndex.ejs', {userItems})   
     } catch (error) {
         console.error('The following error was encountered:' + error)
     }
@@ -44,23 +45,25 @@ router.post('/', async(req,res)=>{
     }
 })
 
-// // Display details page of a single item
-// router.get('/:id', async(req,res)=>{
-//     try {
-//         //logic here
-//     } catch (error) {
-//         console.error('The following error was encountered:' + error)
-//     }
-// })
+// Display details page of a single item
+router.get('/:id', async(req,res)=>{
+    try {
+        const item = await Item.findById(req.params.id).populate('owner')
+        res.render('items/showItem.ejs', {item})
+    } catch (error) {
+        console.error('The following error was encountered:' + error)
+    }
+})
 
-// // Display edit form for a specific item
-// router.get('/:id/edit', async(req,res)=>{
-//     try {
-//         //logic here
-//     } catch (error) {
-//         console.error('The following error was encountered:' + error)
-//     }
-// })
+// Display edit form for a specific item
+router.get('/:id/edit', async(req,res)=>{
+    try {
+        const item = await Item.findById(req.params.id)
+        res.render('items/editItem.ejs', {item})
+    } catch (error) {
+        console.error('The following error was encountered:' + error)
+    }
+})
 
 // // Update wishlist item you own
 // router.put('/:id', async(req,res)=>{
