e1240686350e27ee737d9075a996ded4ba06117d
safakhalaf026 <safakhalaf026@gmail.com>
2025-12-18 09:33:07 +0300

    removed console.log

--- middleware/isSignedIn.js
+++ middleware/isSignedIn.js
@@ -1,9 +1,7 @@
 const isSignedIn = (req, res, next) => {
   if (req.session.user) {
-    console.log('You are logged in and able to see this page!!!!');
     next();
   } else {
-    console.log('Unauthorized!!!');
     res.redirect('/auth/sign-up');
   }
 };
